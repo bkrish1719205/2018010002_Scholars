d1a9d864e702a4ecc7819b584a382a3f8a518049
Balakrishnan N <60445196+bkrish1719205@users.noreply.github.com>
2020-05-25 15:29:08 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,33 @@
+a=float(input("enter the number1:"))
+b=float(input("Enter the number2:"))
+def add (a,b):
+    ans= a + b
+    return ans
+result=add(a,b)
+print('adding values ', result)
+def subtract (a, b):
+    ans= a - b
+    return ans
+result=subtract(a,b)
+print('subtract values', result)
+def multiply (a, b):
+    ans= a * b
+    return ans
+result=multiply(a,b)
+print('multiply values',result)
+def division (a, b):
+    ans= a / b
+    return ans
+result=division(a,b)
+print('division values',result)
+def reminder(a,b):
+    ans=a % b
+    return ans
+result=reminder(a,b)
+print('reminder values',result)
+
+
+
+
+
+
